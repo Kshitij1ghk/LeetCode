5ffaaabd77ce9f577fafc2f643ba8afceaf12b66
Kshitij1ghk <kshitijwk@gmail.com>
2026-04-03 16:55:11 +0530

    Remoive element

---
+++ RemoveElement/RemoveElement/RemoveElement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveElement
+{
+    internal class Program
+    {
+        public int RemoveElement(int[] nums,int val)
+        {
+            int k = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] != val)
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+            return k;
+        }
+        //O(n)
+        //nums = [3, 2, 2, 3], val = 3
+
+//      i=0: nums[0]=3 == val → skip
+//      i = 1: nums[1]=2 != val → nums[0]=2, k=1
+//      i=2: nums[2]=2 != val → nums[1]=2, k=2
+//      i=3: nums[3]=3 == val → skip
+
+//       Result: [2, 2, _, _], return k=2 ✅
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
