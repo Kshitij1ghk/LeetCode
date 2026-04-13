7357d8061d4a9606b91698fae73113cf61dffa54
Kshitij1ghk <kshitijwk@gmail.com>
2026-04-13 18:37:37 +0530

    Length of last word leetcode problem

---
+++ Length of last word/Length of last word/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length_of_last_word
+{
+    internal class Program
+    {
+        public int LengthOfLastWord(string s)
+        {
+            int length = 0;
+            int i = s.Length-1;
+            while(i>=0 && s[i] == ' ')   i--;
+            while(i>=0 && s[i] !=' ')
+            {
+                length++;
+                i--;
+            }
+            return length;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
